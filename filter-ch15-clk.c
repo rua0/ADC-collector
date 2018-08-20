a9fa15a2f09f74e3fcd5438fb5c2cbbaa6dd0656
rua0 <31871471+rua0@users.noreply.github.com>
2018-08-20 10:34:00 -0400

    Wraping up
    fixed a littlee bug in filter program. About to wrap it up for now.

--- filter-ch15-clk.c
+++ filter-ch15-clk.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+//to compile: gcc filter.c -O3 -o filter
+//to add to bin: sudo cp filter /bin/filter
 
 int main() {
 	uint16_t sample;
@@ -21,9 +23,9 @@ int main() {
 		//mask out the 15th bit as the clk
 		clk = sample>>15;
 		//left shift to take out the msb then rescale
-		sig = (sample<<1)>>1;
+		sig = sample&0x7FFF;
 		if (!oldclk && clk) {
-			fwrite(&sample, 2, 1, stdout);
+			fwrite(&sig, 2, 1, stdout);
 		}
 		oldclk=clk;
 	}
